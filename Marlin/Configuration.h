8eb3e2f981221193f929a39a30baee3b0740c9dc
StevilKnevil <stevilknevil@hotmail.co.uk>
2021-02-18 11:05:06 +0000

    Specify min temps for heating

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -522,7 +522,7 @@
 
 // Below this temperature the heater will be switched off
 // because it probably indicates a broken thermistor wire.
-#define HEATER_0_MINTEMP   0
+#define HEATER_0_MINTEMP   5
 #define HEATER_1_MINTEMP   5
 #define HEATER_2_MINTEMP   5
 #define HEATER_3_MINTEMP   5
@@ -530,7 +530,7 @@
 #define HEATER_5_MINTEMP   5
 #define HEATER_6_MINTEMP   5
 #define HEATER_7_MINTEMP   5
-#define BED_MINTEMP        0
+#define BED_MINTEMP        5
 #define CHAMBER_MINTEMP    5
 
 // Above this temperature the heater will be switched off.
