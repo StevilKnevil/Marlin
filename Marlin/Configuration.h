680fed930ee579f077b87b7a726b559d8b8b1e61
StevilKnevil <stevilknevil@hotmail.co.uk>
2023-03-03 17:37:04 +0000

    Updated defaults

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1244,7 +1244,7 @@
  * Override with M201
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 500, 500, 100, 5000 }
+#define DEFAULT_MAX_ACCELERATION      { 1000, 2500, 100, 5000 } // Stevil Knevil
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
@@ -1261,9 +1261,9 @@
  *   M204 I    Angular Acceleration
  *   M204 J    Angular Travel Acceleration
  */
-#define DEFAULT_ACCELERATION                   500  // X, Y, Z ... and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION           1000  // E acceleration for retracts // Stevil Knevil
-#define DEFAULT_TRAVEL_ACCELERATION           1000  // X, Y, Z ... acceleration for travel (non printing) moves
+#define DEFAULT_ACCELERATION                  1200  // X, Y, Z ... and E acceleration for printing moves // Stevil Knevil
+#define DEFAULT_RETRACT_ACCELERATION          1200  // E acceleration for retracts // Stevil Knevil
+#define DEFAULT_TRAVEL_ACCELERATION          15000  // X, Y, Z ... acceleration for travel (non printing) moves // Stevil Knevil
 #if ENABLED(AXIS4_ROTATES)
   #define DEFAULT_ANGULAR_ACCELERATION        3000  // I, J, K acceleration for rotational-only printing moves
   #define DEFAULT_ANGULAR_TRAVEL_ACCELERATION 3000  // I, J, K acceleration for rotational-only travel (non printing) moves
