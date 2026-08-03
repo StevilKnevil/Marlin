f899311a143761ed42f5423c946d587b32f7e212
StevilKnevil <stevilknevil@hotmail.co.uk>
2026-08-03 14:21:44 +0100

    Increase MPC heater power to 40W for improved performance

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -709,7 +709,7 @@
   //#define MPC_AUTOTUNE_MENU                         // Add MPC auto-tuning to the "Advanced Settings" menu. (~350 bytes of flash)
 
   #define MPC_MAX 255                                 // (0..255) Current to nozzle while MPC is active.
-  #define MPC_HEATER_POWER { 30.0f }                  // (W) Heat cartridge powers. // Stevil Knevil
+  #define MPC_HEATER_POWER { 40.0f }                  // (W) Heat cartridge powers. // Stevil Knevil
 
   #define MPC_INCLUDE_FAN                             // Model the fan speed?
 
